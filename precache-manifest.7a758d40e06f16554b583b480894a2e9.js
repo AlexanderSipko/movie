213858830c3639780097de5e527deca228e7152a
Asipko <avsipko@gmail.com>
2023-03-04 23:02:08 +0500

    Updates

---
+++ precache-manifest.7a758d40e06f16554b583b480894a2e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "791766f368834127add5",
+    "url": "/movie/static/css/main.d5376fb4.chunk.css"
+  },
+  {
+    "revision": "791766f368834127add5",
+    "url": "/movie/static/js/main.791766f3.chunk.js"
+  },
+  {
+    "revision": "1ed4279039940183f6f8",
+    "url": "/movie/static/js/1.1ed42790.chunk.js"
+  },
+  {
+    "revision": "31963bdd32f5110fe4df",
+    "url": "/movie/static/js/runtime~main.31963bdd.js"
+  },
+  {
+    "revision": "343a1b53db4dfc4939641c49d8e9c313",
+    "url": "/movie/static/media/movie_background_1.343a1b53.jpg"
+  },
+  {
+    "revision": "937bcf64bc6110484ac68ec6e418a8d3",
+    "url": "/movie/index.html"
+  }
+];
